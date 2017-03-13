cc08ec778883f650d441ad840c7651da2a160f80
easyli <ogg2@qq.com>
2017-03-13 08:41:10 +0800

    7w

--- 7w/5.cpp
+++ 7w/5.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <cstring>
+#include <cstdio>
 using namespace std;
-
-int main(int argc, char const *argv[]){
-
-	return 0;
+char a[10001];
+int main()
+{
+    gets(a);
+    int l=strlen(a);
+    for(int i=0;i<l;i++)
+    {
+        if(a[i]=='z')a[i]='a';
+        else if(a[i]=='Z')a[i]='A';
+        else if((a[i]>='A'&&a[i]<='Z')||(a[i]>='a'&&a[i]<='z'))
+        a[i]=a[i]+1;
+    }
+    puts(a);
+    return 0;
 }
